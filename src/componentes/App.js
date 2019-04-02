b46e2caa9e4a8e600956678142886b93da577ae1
Rafa Tárrega <rafatarrega@cecotec.es>
2019-04-02 18:27:41 +0200

    Video 59 acabado

---
+++ src/componentes/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+import Header from './Header';
+import Formulario from './Formulario';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <Header 
+          titulo = 'Gasto Semanal'
+        />
+
+        <div className="contenido-principal contenido">
+          <div className="row">
+            <div className="one-half column">
+            <Formulario />
+            </div>
+            <div className="one-half column">
+            
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
