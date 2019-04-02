72b5170cf2260a42d5f9760aa19f85c774857054
Rafa Tárrega <rafatarrega@cecotec.es>
2019-04-02 18:50:52 +0200

    clase 61 hecha

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -4,6 +4,27 @@ import Header from './Header';
 import Formulario from './Formulario';
 
 class App extends Component {
+  state = {
+    presupuesto : '',
+    restante : '',
+    gastos: {}
+  }
+
+  //Agregar un nuevo gasto al state
+  agregarGasto = gasto => {
+
+  //Coger una copia del state actual
+    const gastos = {...this.state.gastos};
+    
+    console.log('Se ha agregado el gasto' + gasto);
+    console.log(gastos);
+
+  //Agregar el gasto al objeto del state
+    // this.props.agregarGasto(gasto);
+
+  //Ponerlo en el state
+  }
+
   render() {
     return (
       <div className="App container">
@@ -14,7 +35,9 @@ class App extends Component {
         <div className="contenido-principal contenido">
           <div className="row">
             <div className="one-half column">
-            <Formulario />
+            <Formulario 
+              agregarGasto = {this.agregarGasto}
+            />
             </div>
             <div className="one-half column">
             
